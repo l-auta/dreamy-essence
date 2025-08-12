0c6744ce3febcef801e8ffc68b13b92074ea46c9
lisa <lisaauta018@gmail.com>
2025-08-12 11:22:34 +0300

    removed content i dont need

---
+++ src/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the main content area.</p>
+    </div>
+  );
+}
+
+export default Homepage;
