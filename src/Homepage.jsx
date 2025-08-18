38bbceef990caf4eac583d2f7652757f06cfdca1
lisa <lisaauta018@gmail.com>
2025-08-18 11:38:03 +0300

    left with functionalty 4 navbar

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -5,13 +5,13 @@ function Navbar() {
     return (
         <nav className="bg-white shadow">
             <div className="px-4 sm:px-6 lg:px-8"> {/* for the sake of media responsiveness .....(was there initially causing the nav to be halfway) max-w-7xl*/}
-                <div className="flex items-center justify-between h-16"> {/* the layout of the navbar*/}
+                <div className="flex items-center justify-between h-15"> {/* the layout of the navbar*/}
                     <div className="sm:flex hidden items-center">
                         <img
                             className="h-16 sm:w-18"
                             src="src/assets/cafe Logos-2-Design_two.png"
                             alt="Dreamy Essence Logo"
-                        />
+                        /> 
                         </div>
                    
                         <div className="flex space-x-3 md:space-x-6 md:text-xl font-medium">
